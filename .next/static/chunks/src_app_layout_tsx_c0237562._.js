39305270a53bb7f4563fe0ce4e2fb7b7304fbd62
usdtskyz70 <skyzusdt428@gmail.com>
2025-08-12 23:08:47 +0200

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1755032926937

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_5b188c12._.js",
+  "static/chunks/src_887b3226._.js"
+],
+    source: "dynamic"
+});
