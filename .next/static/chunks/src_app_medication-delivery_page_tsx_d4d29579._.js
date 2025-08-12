8f3c72460b08fbe758e730c3139c03ff3ca65a34
usdtskyz70 <skyzusdt428@gmail.com>
2025-08-12 23:08:28 +0200

    Add .next/static/chunks/src_app_medication-delivery_page_tsx_d4d29579._.js 1755032908566

---
+++ .next/static/chunks/src_app_medication-delivery_page_tsx_d4d29579._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a872c47d._.js",
+  "static/chunks/node_modules_a62a6408._.js"
+],
+    source: "dynamic"
+});
